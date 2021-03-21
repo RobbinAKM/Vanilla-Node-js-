18f6f8491a5acd64488fd6193ce8af5585e1a573
aung min <robbinakm12@hotmail.com>
2021-03-21 22:24:41 +0900

    added CLI for admin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var server = require('./lib/server');
 var workers = require('./lib/workers');
+var cli = require('./lib/cli');
 
 var app= {};
 
@@ -8,6 +9,12 @@ app.init= function(){
 server.init();
 
 workers.init();
+
+// Start the CLI, but make sure it starts last
+setTimeout(function(){
+  cli.init();
+},50);
+
 };
 
 app.init();
